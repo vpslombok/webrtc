961ac6d1a056fd00c65562b6cba546859a4d87ef
vpslombok <136515134+vpslombok@users.noreply.github.com>
2024-09-11 11:21:20 +0800

    Update server.js

--- server.js
+++ server.js
@@ -23,6 +23,19 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('message', data);
     });
 
+    // Handle WebRTC signaling
+    socket.on('offer', (offer) => {
+        socket.broadcast.emit('offer', offer);
+    });
+
+    socket.on('answer', (answer) => {
+        socket.broadcast.emit('answer', answer);
+    });
+
+    socket.on('ice-candidate', (candidate) => {
+        socket.broadcast.emit('ice-candidate', candidate);
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
